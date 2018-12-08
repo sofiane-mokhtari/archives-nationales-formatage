fd82660582c092c52f0a4f9502f4bec9ce840b42
sofiane-mokhtari <35307774+sofiane-mokhtari@users.noreply.github.com>
2018-12-08 22:19:06 +0100

    Update csv_to_json.py

--- csv_to_json.py
+++ csv_to_json.py
@@ -7,13 +7,11 @@ def remove_accents(input_str):
 	return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
 def create_json_discours() :
-	i = 0;
-	with open('data.json', 'w', encoding='utf8') as outfile:
+	with open('Discours.json', 'w', encoding='utf8') as outfile:
 		with open('03_le_politique_parle_au_citoyen\Rocard - Allocutions - Inventaire\FRAN_IR_050330_Rocard_allocutions.csv', newline='') as csvfile:
 			spamreader  = csv.reader(csvfile, delimiter=';')
 			try:
 				for row in spamreader:
-					if (i != 0) :
 						lieux = remove_accents((row[5].split(","))[0])
 						if (lieux != "S. l."):
 							departement = lieux.split('(')
@@ -24,24 +22,18 @@ def create_json_discours() :
 							except Exception as e:
 								print (e)
 							ret = r.json()
-							if not ret :
-								coo = -888
-							else :
-								coo = ret[0]['centre']['coordinates']
+							if ret :
 								my_row ={
-									"id": row[0],
-									"uni-id": row[1],
+									"id": row[7] + "-" + lieux,
 									"date": row[7],
 									"lieux": lieux,
-									"cooo": coo,
-									"typologie": remove_accents(row[6])
-									"path": row[:]
+									"geocalisation": ret[0]['centre']['coordinates'],
+									"typologie": remove_accents(row[6]),
+									"path": row[10]
 									}
 								out = json.dumps(my_row, indent=4, sort_keys=True, separators=(',', ': '), ensure_ascii=False)
 								outfile.write(out)
 								outfile.write("\n")
-					else :
-						i = 1
 			except Exception as e:
 				print (e)
 
